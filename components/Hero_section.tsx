ec88b4aa3f59fa1da31dfb3a3c49bf52fbe99729
hania-1 <160575495+hania-1@users.noreply.github.com>
2024-10-15 18:28:05 +0500

    Update Hero_section.tsx

--- components/Hero_section.tsx
+++ components/Hero_section.tsx
@@ -9,11 +9,11 @@ export const Hero_section = () => {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className=" text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I&#39;m {/* Escaping the single quote */}
+              Hello, I&apos;m {/* Use &apos; for the apostrophe */}
             </span>
-            <br></br>
+            <br />
             <TypeAnimation
               sequence={[
                 "Hania!",
@@ -31,12 +31,12 @@ export const Hero_section = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl ">
-            Hi, my name is Hania, and I'm a passionate Frontend Developer with
+            Hi, my name is Hania, and I&apos;m a passionate Frontend Developer with
             a keen interest in creating modern and user-friendly web
-            experiences. Currently, I’m focused on enhancing my skills in web
+            experiences. Currently, Im focused on enhancing my skills in web
             development while learning new technologies in IT. I enjoy building
             creative projects and finding solutions to everyday problems through
-            code{" "}
+            code.
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-300 text-white">
@@ -61,3 +61,4 @@ export const Hero_section = () => {
 };
 
 export default Hero_section;
+
